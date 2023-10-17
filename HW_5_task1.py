b36a54e4873f60dca8e9707e5ebe1dc5328d5274
Iryna Tsymbaliuk <irene.tsymb@gmail.com>
2023-10-17 22:14:48 +0300

    Homework #5

--- HW_5_task1.py
+++ HW_5_task1.py
@@ -1,9 +1,11 @@
 num = input('Введіть число ', )
 n = list(num)
+
 for i, l in enumerate(n):
         print(i, l, n[(i+1):])
         if l in n[(i+1):] :
             print('Так')
             break
+#        i = i+1
 else:
     print('Ні')
