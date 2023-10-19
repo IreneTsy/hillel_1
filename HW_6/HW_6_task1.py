49a364c7dec08fb58920e6ed090fca2376f61255
Iryna Tsymbaliuk <irene.tsymb@gmail.com>
2023-10-19 19:25:05 +0300

    Homework directory#6

--- HW_6/HW_6_task1.py
+++ HW_6/HW_6_task1.py
@@ -1,5 +1,5 @@
 suma = int(input('Enter the amount '))
-term = int(input('Chщose the term - 1 or 5 years '))
+term = int(input('Choose the term - 1 or 5 years '))
 term_month = term*12
 term_overall = term*12
 print('|', 'Month'.center(3), '|',
@@ -14,17 +14,17 @@ if term == 1 or term == 5:
             perc_per_month = suma*0.02
             suma = suma - (suma/term_overall)
             term_overall = term_overall - 1 
-            print('|',("%.0f"%term_month).center(5), '|',
-          ("%.2f"%suma_per_month).ljust(15), '|',
-          ("%.2f"%perc_per_month).ljust(17), '|',
-          ("%.2f"%suma).ljust(11), '|'.ljust(5))
+            print('|',("%.0f"%term_month).center(len('Month')), '|',
+          ("%.2f"%suma_per_month).ljust(len('Monthly payment')), '|',
+          ("%.2f"%perc_per_month).ljust(len('Percent per month')), '|',
+          ("%.2f"%suma).ljust(len('Debt to pay')), '|'.ljust(5))
         else:
             #percent = '4%'
             suma_per_month = (suma/term_overall)+(suma*0.04)
             perc_per_month = suma*0.04
             suma = suma - (suma/term_overall)
             term_overall = term_overall - 1
-            print('|',("%.0f"%term_month).center(5), '|',
-          ("%.2f"%suma_per_month).ljust(15), '|',
-          ("%.2f"%perc_per_month).ljust(17), '|',
-          ("%.2f"%suma).ljust(11), '|'.ljust(5))
+            print('|',("%.0f"%term_month).center(len('Month')), '|',
+          ("%.2f"%suma_per_month).ljust(len('Monthly payment')), '|',
+          ("%.2f"%perc_per_month).ljust(len('Percent per month')), '|',
+          ("%.2f"%suma).ljust(len('Debt to pay')), '|'.ljust(5))
