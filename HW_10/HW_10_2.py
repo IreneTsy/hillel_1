275fe806433e32e593ea780c1e027305fa7f240f
Iryna Tsymbaliuk <irene.tsymb@gmail.com>
2023-11-02 10:29:08 +0200

    HW #9

---
+++ HW_10/HW_10_2.py
@@ -0,0 +1,45 @@
+import random
+
+"Задаємо на вхід довільні параметри"
+def create_2d_arrays(*args):
+    "Ф-ція якщо параметр не введений"
+    if len(args) == 0:
+        print(None)
+"Ф-ція для одного параметру"
+    elif len(args) == 1:
+"""Генерація 2 вимірного списку з одним параметром, що заповнені
+ випадковими цілими значенням в диапазлні 0 - 200"""
+        matrix = [ 
+          [random.randint(0, 200) for _ in range(args[0])]
+            for __ in range(args[0])
+        ]
+"""Визначення максимальної довжини рядка для формувння симетричної таблички"""
+        max_len = max(len(str(max(row)) if row else 0) for row in matrix)
+        for row in matrix:
+            formatted_row = [f'{val:^{max_len}}' for val in row]
+"Виведення відформатованого списку по рядкам"
+            print(' '.join(formatted_row))
+"Ф-ція для двох параметрів"
+    elif len(args) == 2:
+        matrix = [
+            [random.randint(0, 200) for _ in range(args[0])]
+            for __ in range(args[1])
+        ]
+"""Визначення максимальної довжини рядка для формувння симетричної таблички"""
+        max_len = max(len(str(max(row)) if row else 0) for row in matrix)
+        for row in matrix:
+            formatted_row = [f'{val:^{max_len}}' for val in row]
+            print(' '.join(formatted_row))
+
+
+"""2 га функція очікує один обовязковий парметр і це має бути
+2 вимірний список симетричний"""
+def create_matrix(n):
+        matrix = [ 
+          [random.randint(0, 200) for _ in range(n)]
+            for __ in range(n)
+        ]
+        max_len = max(len(str(max(row)) if row else 0) for row in matrix)
+        for row in matrix:
+            formatted_row = [f'{val:^{max_len}}' for val in row]
+            print(' '.join(formatted_row))
