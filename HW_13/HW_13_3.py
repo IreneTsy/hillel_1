3d5710cf0079d3fa2100336f35be0de29d2bae92
Iryna Tsymbaliuk <irene.tsymb@gmail.com>
2023-11-09 16:25:59 +0200

    HW #13

---
+++ HW_13/HW_13_3.py
@@ -0,0 +1,9 @@
+# not ready yet
+lst_hero = ()
+with open ('hero.ini', 'w', encoding='utf-8') as handler:
+    print(type(handler))
+    #for line in handler:
+        #lst_hero.append(line)
+        #print(lst_hero)
+
+    handler.close()
